16337cf2e928a199c4fdca06ff22f268fea15f6c
Fra-Tera04 <francescorogo22@gmail.com>
2024-05-15 12:51:47 +0200

    add ConcorrentiUtenteAsta Entity

---
+++ DietiDeals24Backend/src/main/java/entità/ConcorrentiUtenteAsta.java
@@ -0,0 +1,27 @@
+package entità;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ConcorrentiUtenteAsta")
+public class ConcorrentiUtenteAsta 
+{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_Utnete", nullable = false)
+	private int ID_Utnete;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_Asta", nullable = false)
+	private int ID_Asta;
+
+	// forse ci possiamo add degli attributi per sapere qualcosa in piu?
+}
